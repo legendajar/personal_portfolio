32cbc23aa8f3e83084b57e6e8e26b558c2ebe554
legendajar <harshsolanki9898@gmail.com>
2023-08-01 03:02:19 +0530

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,27 @@
-/*=============== SHOW MENU ===============*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
 
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/*=============== SWIPER PROJECTS ===============*/
+/* Link active work */ 
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 
-/*=============== EMAIL JS ===============*/
-
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== LIGHT DARK THEME ===============*/ 
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
